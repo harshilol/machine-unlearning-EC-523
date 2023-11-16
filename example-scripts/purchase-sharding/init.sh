0b10abfeeb518b2e82fb6156567bafcb3c648126
GM Harshvardhan <54180159+GM-git-dotcom@users.noreply.github.com>
2023-11-15 21:44:16 -0500

    Update init.sh for SCC compatibility
    Fixed bash commands to work with absolute paths on SCC

--- example-scripts/purchase-sharding/init.sh
+++ example-scripts/purchase-sharding/init.sh
@@ -5,12 +5,12 @@ IFS=$'\n\t'
 
 shards=$1
     
-if [[ ! -d "containers/${shards}" ]] ; then
-    mkdir "containers/${shards}"
-    mkdir "containers/${shards}/cache"
-    mkdir "containers/${shards}/times"
-    mkdir "containers/${shards}/outputs"
-    echo 0 > "containers/${shards}/times/null.time"
+if [[ ! -d "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}" ]] ; then
+    mkdir "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}"
+    mkdir "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}/cache"
+    mkdir "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}/times"
+    mkdir "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}/outputs"
+    echo 0 > "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}/times/null.time"
 fi
 
 python distribution.py --shards "${shards}" --distribution uniform --container "${shards}" --dataset datasets/purchase/datasetfile --label 0
