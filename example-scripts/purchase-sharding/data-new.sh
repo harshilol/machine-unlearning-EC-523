8bc7a9e747f4856236e178a27820418880ada48f
GM Harshvardhan <54180159+GM-git-dotcom@users.noreply.github.com>
2023-11-15 21:45:58 -0500

    Working version of result creation in data.sh
    Fixed wildcard matching issue in `cat` command and other path problems specifically to work with BU's SCC

---
+++ example-scripts/purchase-sharding/data-new.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eou pipefail
+IFS=$'\n\t'
+
+shards=$1
+
+if [[ ! -f general-report.csv ]]; then
+    echo "nb_shards,nb_requests,accuracy,retraining_time" > general-report.csv
+fi
+
+for j in {0..15}; do
+    r=$((${j}*${shards}/5))
+    acc=$(python aggregation.py --strategy uniform --container "${shards}" --shards "${shards}" --dataset datasets/purchase/datasetfile --label "${r}")
+    #cat /containers/"${shards}"/times/shard-*:"${r}".time > "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}/times/times"
+    cat "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}/times/shard-0:${r}.time" > "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}/times/times"
+    cat "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}/times/shard-1:${r}.time" > "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}/times/times"
+    cat "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}/times/shard-2:${r}.time" > "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}/times/times"
+    cat "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}/times/shard-3:${r}.time" > "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}/times/times"
+    cat "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}/times/shard-4:${r}.time" > "/projectnb/ec523kb/students/gmharsh/unlearning/machine-unlearning/containers/${shards}/times/times"
+    time=$(python time.py --container "${shards}" | awk -F ',' '{print $1}')
+    echo "${shards},${r},${acc},${time}" >> general-report.csv
+
+done
